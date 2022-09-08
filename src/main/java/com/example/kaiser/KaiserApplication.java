0478bcf77bee2af8c8f1e3e49a8ebcc943ced2cd
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-09-08 11:13:50 +0900

    first commit

---
+++ src/main/java/com/example/kaiser/KaiserApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kaiser/KaiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
